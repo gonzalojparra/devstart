14041dd66f8170ddd0e97784afb391af40725bc0
Gonzalo Parra <gonzzaparra@gmail.com>
2024-01-31 22:14:16 -0300

    feat(core): add not found page and update layout on job page

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 
 import prisma from '@/lib/db';
 
+import { Button } from '@/components/ui/button';
 import JobPage from './components/job-page';
 
 interface PageProps {
@@ -20,6 +21,15 @@ const getJob = cache(async (slug: string) => {
   return job;
 });
 
+export async function generateStaticParams() {
+  const jobs = await prisma.job.findMany({
+    where: { approved: true },
+    select: { slug: true }
+  });
+
+  return jobs.map(({ slug }) => ({ params: { slug } }));
+}
+
 export async function generateMetadata({
   params: { slug }
 }: PageProps): Promise<Metadata> {
@@ -34,9 +44,31 @@ export default async function Page({
 }: PageProps) {
   const job = await getJob(slug);
 
+  const { applicationEmail, applicationUrl } = job;
+
+  const applicationLink = applicationEmail
+    ? `mailto:${applicationEmail}`
+    : applicationUrl;
+
+  if (!applicationLink) {
+    console.error('Job has no application link or email');
+    notFound();
+  }
+
   return (
     <main className='max-w-5xl px-3 m-auto my-10 flex flex-col md:flex-row items-center gap-5 md:items-start'>
       <JobPage job={job} />
+      <aside>
+        <Button asChild>
+          <a
+            href={applicationLink}
+            className='w-40 block md:w-fit'
+            target='_blank'
+          >
+            Apply
+          </a>
+        </Button>
+      </aside>
     </main>
   )
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <main className='max-w-5xl m-auto my-10 space-y-5 px-3 text-center'>
+      <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none'>
+        Not Found
+      </h1>
+      <p>The page you are looking for doesn't exist.</p>
+    </main>
+  )
+}
